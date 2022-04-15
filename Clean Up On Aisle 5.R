3707d9b2f649daa9da59b87f4dd1235158633a62
RooIrons <bexirons@gmail.com>
2022-04-15 12:19:18 +0200

    Commit with Rmd file & changes

--- Clean Up On Aisle 5.R
+++ Clean Up On Aisle 5.R
@@ -11,7 +11,7 @@ rm(list=ls())
 install.packages("tidyverse")
 
 #To access the data from SABAP/SAFRING using an api
-install.packages("Rcurl")
+install.packages("RCurl")
 install.packages("rjson")
 
 ### Step 3: load the packages that are required 
@@ -27,3 +27,28 @@ SPP = 758
 
 #Read in the data
 ringing_data <- read.csv(paste('https://api.birdmap.africa/safring/species/records_list/',SPP,'?format=csv',sep=""))
+
+### Step 5: make the ages numeric and filter the juveniles from the adult (only working with adults)  
+
+ringing_data$Age <- as.numeric(ringing_data$Age)
+ringing_data <- filter(ringing_data, Age>3)
+
+### STep 6: 
+# ensure all metric data is numeric
+ringing_data$Mass <- as.numeric(as.character(ringing_data$Mass))
+ringing_data$Wing <- as.numeric(as.character(ringing_data$Wing))
+ringing_data$Culmen <- as.numeric(as.character(ringing_data$Culmen)) 
+ringing_data$Head <- as.numeric(as.character(ringing_data$Head))
+ringing_data$Tarsus <- as.numeric(as.character(ringing_data$Tarsus))
+
+### Step7: 
+# fix a common issue: 0 is entered when value should be blank (these values cannot be 0)
+# SAFRING: create sql to fix these in your database: these will not be flagged 
+# in the biometric checks
+ringing_data$Mass[ringing_data$Mass==0] <- NA
+ringing_data$Wing[ringing_data$Wing==0] <- NA
+ringing_data$Head[ringing_data$Head==0] <- NA
+ringing_data$Culmen[ringing_data$Culmen==0] <- NA
+ringing_data$Tarsus[ringing_data$Tarsus==0] <- NA
+ringing_data$Tail[ringing_data$Tail==0] <- NA
+
